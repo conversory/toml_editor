14743266c665c27cee7861b8a40aaa98a490aa1d
conversory <502978559@qq.com>
2022-05-23 23:13:44 +0800

    Update toml_editor.py

--- toml_editor.py
+++ toml_editor.py
@@ -18,23 +18,26 @@ sg.theme('GreenTan')
 
 class window_show:
     def __init__(self, list1):
+        self.config = list1
         self.list1 = []
         for i in list1:
             self.list1.append(i)
+        self.list1.append("")
         print(self.list1)
+        self.list2 = []
         self.window = self.generate_window_main()
         pass
 
     def generate_window_main(self):
-        a = sg.Listbox(self.list1, size=(20, 12), key='-LIST', enable_events=True)
+        self.a = sg.Listbox(self.list1, size=(20, 12), key='-LIST-', enable_events=True)
+        self.b = sg.Listbox(self.list2, size=(20, 12), key='sublist')
+        self.col1 = Column([[sg.Button('刷新', font=('宋体', 18))],
+                            [sg.Button('增加', font=('宋体', 18))],
+                            [sg.Button('修改', font=('宋体', 18))],
+                            [sg.Button('删除', font=('宋体', 18))]])
 
-        col1 = Column([[sg.Button('刷新', font=('宋体', 18))],
-                       [sg.Button('增加', font=('宋体', 18))],
-                       [sg.Button('修改', font=('宋体', 18))],
-                       [sg.Button('删除', font=('宋体', 18))]])
-
-        layout = [[col1, a]]
-        return Window('配置文件修改', layout)
+        self.layout = [[self.col1, self.a, self.b]]
+        return Window('配置文件修改', self.layout)
 
     def run(self):
         while True:
@@ -42,9 +45,48 @@ class window_show:
             print(event, values)
             if event == sg.WIN_CLOSED:
                 break
+            if event == '修改':
+                try:
+                    temp = self.config[values['-LIST-'][0]]
+                    print(type(temp))
+                    print(type(type(temp)))
+                    try:
+                        temp2 = self.config[values['-LIST-'][0]][temp]
+                    except:
+                        sg.popup('最后一级')
+                        pass
+                    else:
+                        aa = window_show(self.config[values['-LIST-'][0]])
+                        aa.run()
+                except:
+                    sg.popup('最后一级')
+                    pass
             if event == '增加':
-                aa = window_show(self)
-                aa.run()
+                if values['-LIST-'][0] == "":
+                    temp = sg.popup_get_text('输入增加值')
+                    # print(temp)
+                    self.list1[-1] = temp
+                    self.list1.append("")
+                    print(self.list1)
+                else:
+                    pass
+            if event == '-LIST-':
+                self.list2 = []
+                a = values['-LIST-'][0]
+                try:
+                    for i in self.config[a]:
+                        try:
+                            temp = self.config[a][i]
+                        except:
+                            self.list2.append(self.config[a])
+                            break
+                        else:
+                            self.list2.append(i)
+                except:
+                    self.list2 = []
+                # print(self.list2)
+                self.window['sublist'].update(values=self.list2)
+
         self.window.close()
 
     def refresh(self):
